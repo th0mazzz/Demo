2f5bd44530acdc16c246a0dfca9d74420b42ba01
Thomas <tzhao2@stuy.edu>
2018-01-04 22:23:22 -0500

    Wrote getters and setters

---
+++ Assignment.java
@@ -0,0 +1,15 @@
+public class Assignment{
+
+    private double grade = 0.0;
+    private String name; = "Untitled";
+    private String date = "00/00/0000"
+    
+    public double getGrade(){return grade;}
+    public String getName(){return name;}
+    public String getDate(){return date;}
+
+    private setGrade(double updatedGrade){grade = updatedGrade;}
+    private setName(String updatedName){name = updatedName;}
+    private setDate(String updatedDate){date = updatedDate;}
+    
+}
