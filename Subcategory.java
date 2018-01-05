30926fab70e86e80991afee8f048dd9ac82041ff
Thomas <tzhao2@stuy.edu>
2018-01-05 14:47:27 -0500

    Began subcategory

---
+++ Subcategory.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+public class Subcategory{
+
+    private double average = 100.0;
+    private String name = "Untitled";
+    private double weight = 100.0;
+
+    public Subcategory(double average, String name, double weight){
+	this.average = average;
+	this.name = name;
+	this.weight = weight;
+	
+    }
+    
+    private ArrayList<Assignment> collection = new ArrayList<>();
+    
+    public static void main(String[] args){
+	
+    }
+}
