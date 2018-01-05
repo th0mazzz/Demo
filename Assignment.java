6c937f69c22df5a738de7451f84bc25f2def36f2
Thomas <tzhao2@stuy.edu>
2018-01-04 22:26:06 -0500

    Gave Assignment a constructor

--- Assignment.java
+++ Assignment.java
@@ -1,5 +1,11 @@
 public class Assignment{
 
+    public Assignment(double grade, String name, String date){
+	this.grade = grade;
+	this.name = name;
+	this.date = date;
+    }
+    
     private double grade = 0.0;
     private String name; = "Untitled";
     private String date = "00/00/0000"
