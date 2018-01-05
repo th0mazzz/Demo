786a6df1e825e6014fc9d206b8d1d5f9b34ecc0c
Thomas <tzhao2@stuy.edu>
2018-01-04 22:29:48 -0500

    Put in return types and fixed ;'s

--- Assignment.java
+++ Assignment.java
@@ -7,15 +7,15 @@ public class Assignment{
     }
     
     private double grade = 0.0;
-    private String name; = "Untitled";
-    private String date = "00/00/0000"
+    private String name = "Untitled";
+    private String date = "00/00/0000";
     
     public double getGrade(){return grade;}
     public String getName(){return name;}
     public String getDate(){return date;}
 
-    private setGrade(double updatedGrade){grade = updatedGrade;}
-    private setName(String updatedName){name = updatedName;}
-    private setDate(String updatedDate){date = updatedDate;}
+    private void setGrade(double updatedGrade){grade = updatedGrade;}
+    private void setName(String updatedName){name = updatedName;}
+    private void setDate(String updatedDate){date = updatedDate;}
     
 }
