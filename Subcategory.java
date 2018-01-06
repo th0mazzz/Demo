f68b33dcc907e8b85c1bfb0ac1eaa639a5aeda11
Thomas <tzhao2@stuy.edu>
2018-01-06 18:40:09 -0500

    Added average calculation function

--- Subcategory.java
+++ Subcategory.java
@@ -32,6 +32,16 @@ public class Subcategory{
 	    }
 	}
     }
+
+    public double calcAverage(){
+	double sumOfGrades = 0.0;
+	for(int index = 0; index < collection.size(); index++){
+	    sumOfGrades = sumOfGrades + collection.get(index).getGrade();
+	    System.out.println(sumOfGrades);
+	}
+	return sumOfGrades / collection.size();
+    }
+	    
     
     public String toString(){
 	String returnString = "[";
@@ -54,10 +64,15 @@ public class Subcategory{
 	System.out.println(testOne.getAverage()+ ", " + testOne.getWeight() + ", " + testOne.getName());
 	System.out.println(testOne);
         testOne.addAssignment(99.0, "Exam 1", "01/01/2018");
-	testOne.addAssignment(88.0, "Exam 2", "01/02/2018");
+	testOne.addAssignment(100.0, "Exam 2", "01/02/2018");
 	System.out.println(testOne);
 	testOne.deleteAssignment(99.0, "Exam 1", "01/01/2018");
 	System.out.println(testOne);
+	testOne.addAssignment(80.0, "Exam 3", "01/02/2018");
+	testOne.addAssignment(81.0, "Exam 4", "01/02/2018");
+	testOne.addAssignment(99.0, "Exam 5", "01/02/2018");
+	System.out.println(testOne);
+	System.out.println(testOne.calcAverage());
 	System.out.println("end");
        
     }
