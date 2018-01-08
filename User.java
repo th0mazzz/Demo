c607b77e8f1d36103538a8805025bf0c39d10f0e
Meredith <mhu2@stuy.edu>
2018-01-08 15:47:03 -0500

    filled this out a bit more, with some basic methods

--- User.java
+++ User.java
@@ -1,10 +1,44 @@
+import java.util.ArrayList;
 public class User{
+    public static void main(String[] args){
+	User foo = new User("Bob");
+	System.out.println(foo);
+	Subject a = new Subject("Precalc");
+	foo.addSubject(a);
+	System.out.println(foo);
+    }
+    
     private String name;
     private ArrayList<Subject> classes = new ArrayList<>();
-    private String password;
+    //private String password;
     
     public User(){
 	name="Untitled";
-	password="";
+	//password="";
+    }
+    public User(String foo){
+	name=foo;
+    }
+
+    public String toString(){
+	String c = "";
+	for(int i=0;i<classes.size();i++){
+	    c+=classes.get(i);
+	}
+	return "Name: "+name+
+	    "\nClasses: "+c;
+    }
+    public String getName(){
+	return name;
+    }
+    public void setName(String newName){
+	name=newName;
+    }
+    
+    public void addSubject(Subject x){
+	classes.add(x);
+    }
+    public void rmSubject(Subject n){
+	classes.remove(n);
     }
 }
