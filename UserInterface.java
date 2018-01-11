6e28edc92aa3ae2f1e4c3b16765d2b25351bee20
Meredith <mhu2@stuy.edu>
2018-01-11 15:46:55 -0500

    modified the GUI a bit, made it bigger and added an icon (working on that)

--- UserInterface.java
+++ UserInterface.java
@@ -29,22 +29,32 @@ public class UserInterface extends JFrame{ //needs to implement ActionListener
 
 	this.setUpHomePane();
 	this.setContentPane(homePane);
+	this.setLocationRelativeTo(null);
+	this.setDefaultLookAndFeelDecorated(true);
+	this.setIconImage(new ImageIcon("https://d30y9cdsu7xlg0.cloudfront.net/png/129365-200.png").getImage());
     }
 
     public void setUpHomePane(){
 	homePane = new JPanel(new BorderLayout());
 
+	/*
+	JLabel background = new JLabel("");
+	background.setOpaque(true);
+	background.setPreferredSize(new Dimension(175, 100));
+	homePane.add(background, BorderLayout.CENTER);
+	*/
+
 	JLabel SaturnGrades = new JLabel("Saturn Grades");
 	SaturnGrades.setOpaque(true);
 	SaturnGrades.setBackground(new Color(124, 132,142));
-	SaturnGrades.setPreferredSize(new Dimension(40, 40));
-	homePane.add(SaturnGrades);
+	SaturnGrades.setPreferredSize(new Dimension(200, 200));
+	homePane.add(SaturnGrades, BorderLayout.CENTER);
 
 	JLabel UserName = new JLabel("Name: "+user.getName());
 	UserName.setOpaque(true);
 	UserName.setBackground(new Color(124,135,124));
-	UserName.setPreferredSize(new Dimension(40, 40));
-	homePane.add(UserName);	
+	UserName.setPreferredSize(new Dimension(200, 200));
+	homePane.add(UserName, BorderLayout.PAGE_END);	
     }
 
     public static void main(String[] args){
