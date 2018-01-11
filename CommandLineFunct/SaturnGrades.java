59b4fecef7f72b717b450865eff62625b5bb33ab
Thomas <tzhao2@stuy.edu>
2018-01-11 10:56:45 -0500

    Made a SaturnGrades in directory

---
+++ CommandLineFunct/SaturnGrades.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+public class SaturnGrades{
+
+
+
+    public static void main(String[] args){
+	
+
+
+    }
+}
+
+/*
+arg 0 possibilities
+  subjectName
+  subcategoryName
+  assignmentName
+  addSubject
+  removeSubject
+  add
+
+
+
+
+-----------------------------------
+  arg0 is keyword
+if three args, making assignment
+  arg1 is variable info grade
+  arg2 is variable info name
+  arg3 is variable info date
+
+
+if two args
+  arg1 is subcategory name
+  arg2 is subcategory weight
+
+if one arg
+  arg1 is subject name
+
+
+ */
