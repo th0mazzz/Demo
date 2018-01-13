0095262b8cfee778eb51f2fb8372ab4100f65354
Thomas <tzhao2@stuy.edu>
2018-01-12 19:57:29 -0500

    Made small changes to getAverage() functions to round; working on write function

--- CommandLineFunct/readDriver.java
+++ CommandLineFunct/readDriver.java
@@ -2,9 +2,9 @@ public class readDriver{
     public static void main(String[] args){
 
 	SaturnGrades user = new SaturnGrades();
-	user.readFile();
-	user.writeFile();
-
+	//user.readFile();
+	//user.writeFile();
+	System.out.println(user.write());
 
 
 
