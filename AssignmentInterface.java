a89df99803b04bf3d48030345224726ac134e3d7
Meredith Hu <humeiyin@gmail.com>
2018-01-15 00:14:10 -0500

    oh yeah also finally wrote assignment interface

---
+++ AssignmentInterface.java
@@ -0,0 +1,32 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class AssignmentInterface extends JPanel implements ActionListener{
+    private SaturnGradesGUI frame;
+    private SubcatInterface backpane;
+    private Assignment data;
+    
+    public AssignmentInterface(SaturnGradesGUI frame, SubcatInterface pane, Assignment data){
+	setLayout(new BorderLayout());
+	this.frame=frame;
+	backpane=pane;
+	this.data=data;
+	addBackButton();
+    }
+
+    public void addBackButton(){
+	JButton back = new JButton("Go Back");
+	back.setActionCommand("go_back");
+	back.addActionListener(this);
+	this.add(back, BorderLayout.SOUTH);
+    }
+
+    public void actionPerformed(ActionEvent e){
+	String command = e.getActionCommand();
+	if("go_back".equals(command)){
+	    frame.setContentPane(backpane);
+	    revalidate();
+	}
+    }
+}
